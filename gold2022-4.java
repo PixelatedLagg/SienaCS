c5017d9457afb3bf51e72f17c186bf55bac80201
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-04-04 23:56:33 -0400

    trying rewrite #2

--- gold2022-4.java
+++ gold2022-4.java
@@ -4,7 +4,7 @@ class Program
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         //String target = sc.nextLine();
         String target = "SCOUT";
         int num = 3;
@@ -15,53 +15,83 @@ class Program
         {
             guesses[i] = sc.nextLine();
         }
-        
         sc.close();
-        for (int i = 0; i < num; i++)
+        for (int i = 0; i < num; i++) //iterate over all guesses
         {
-            if (guesses[i] == target)
+            if (guesses[i].equals(target)) //guess equals target
             {
                 System.out.println("GGGGG");
-                return;
+                continue;
             }
-            Boolean[] filled = new Boolean[5];
-            for (int x = 0; x < 5; x++)
+            for (int j = 0; j < 5; j++) //iterate over chars in guess
             {
-                filled[x] = false;
-            }
-            for (int j = 0; j < 5; j++)
-            {
-                Boolean output = false;
-                if (guesses[i].charAt(j) == target.charAt(j))
+                if (guesses[i].charAt(j) == target.charAt(j)) //char is in right position
                 {
                     System.out.print("G");
-                    filled[j] = true;
                     continue;
                 }
-                for (int k = 0; k < 5; k++)
+                else if (target.indexOf(guesses[i].charAt(j)) != -1) //char is contained in target
                 {
-                    if (filled[k])
+                    
+                }
+            }
+        }*/
+        Scanner sc = new Scanner(System.in);
+        String target = sc.nextLine();
+        while (guess.Length > 5)
+        {
+            Console.CursorTop = cursorTopText;
+            Console.Write($"Guess: {new string(' ', guess.Length)}");
+            Console.CursorLeft -= guess.Length;
+            guess = Console.ReadLine() ?? "";
+        }
+        Console.CursorTop = cursorTop + i;
+        if (guess == target)
+        {
+            correct = true;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"  {guess[0]}    {guess[1]}    {guess[2]}    {guess[3]}    {guess[4]}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"You've guessed the word in {i + 1} {(i == 0 ? "try" : "tries")}!");
+            return;
+        }
+        bool[] filled = new bool[5];
+        for (int j = 0; j < 5; j++)
+        {
+            bool output = false;
+            if (guess[j] == target[j])
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"  {guess[j]}  ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                filled[j] = true;
+                continue;
+            }
+            for (int k = 0; k < 5; k++)
+            {
+                if (filled[k])
+                {
+                    continue;
+                }
+                if (target[k] == guess[j])
+                {
+                    if (target[k] == guess[k])
                     {
                         continue;
                     }
-                    if (target.charAt(k) == guesses[i].charAt(j))
-                    {
-                        if (target.charAt(k) == guesses[i].charAt(j))
-                        {
-                            continue;
-                        }
-                        System.out.print("Y");
-                        filled[k] = true;
-                        output = true;
-                        break;
-                    }
-                }
-                if (!output)
-                {
-                    System.out.print("D");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"  {guess[j]}  ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    filled[k] = true;
+                    output = true;
+                    break;
                 }
             }
-            System.out.println();
+            if (!output)
+            {
+                Console.Write("  #  ");
+            }
         }
+        Console.WriteLine();
     }
 }
